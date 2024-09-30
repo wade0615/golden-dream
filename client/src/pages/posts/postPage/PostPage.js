e56bcae6d75aef87d73908f59f58126c1dda2a6f
wade17 <wade_wu@17life.com>
2024-09-30 17:30:31 +0800

    feat: MarkdownEditor

--- client/src/pages/posts/postPage/PostPage.js
+++ client/src/pages/posts/postPage/PostPage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import MarkdownEditor from '@uiw/react-markdown-editor';
 // import { useNavigate } from 'react-router-dom';
 // import localStorageUtil from 'utils/localStorageUtil';
 // import LocalStorageKeys from 'constants/localStorageKeys';
@@ -15,6 +16,10 @@ const _EHS = new ExceptionHandleService({
 
 /** 文章頁  */
 const PostPage = () => {
+  const mdStr = `# This is a H1  \n## This is a H2  \n###### This is a H6`;
+
+  const [markdown, setMarkdown] = useState(mdStr);
+
   /** 初次載入 */
   const getInit = useCallback(async () => {
     try {
@@ -32,6 +37,16 @@ const PostPage = () => {
   return (
     <div id='about' className='about_container'>
       PostPage
+      <MarkdownEditor
+        value={markdown}
+        height='200px'
+        onChange={(value, viewUpdate) => {
+          console.log('value:', value);
+          setMarkdown(value);
+        }}
+      />
+      <hr />
+      <MarkdownEditor.Markdown source={mdStr} height='200px' />
     </div>
   );
 };
