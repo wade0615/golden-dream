a17f3f89ad4f54c21a84a63e507f7a5f7a262dd3
wade17 <wade_wu@17life.com>
2024-11-26 01:54:26 +0800

    feat: 新版 loading 樣式/更換 layout 區字型

--- client/src/components/loading/Loading.js
+++ client/src/components/loading/Loading.js
@@ -1,16 +1,34 @@
-import Spinner from 'react-bootstrap/Spinner';
+// import Spinner from 'react-bootstrap/Spinner';
 import './loadingStyle.scss';
 
 function Loading({ isLoading = true }) {
   if (!isLoading) return null;
   return (
-    <div className='loading-container'>
-      <Spinner
+    <div className='loading_container'>
+      {/* <Spinner
         animation='border'
         role='status'
         style={{ width: '3rem', height: '3rem' }}
       />
-      <p> Loading...</p>
+      <p> Loading...</p> */}
+      <div class='loader'>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
 }
