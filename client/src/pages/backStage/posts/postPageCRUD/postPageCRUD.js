c77c87794006650b7ad4b65b4c076b3cde1700b2
wade17 <wade_wu@17life.com>
2024-12-12 01:36:35 +0800

    feat: PostPageCRUD with Form

--- client/src/pages/backStage/posts/postPageCRUD/postPageCRUD.js
+++ client/src/pages/backStage/posts/postPageCRUD/postPageCRUD.js
@@ -1,9 +1,18 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import MarkdownEditor from '@uiw/react-markdown-editor';
 import { useLocation, useNavigate } from 'react-router-dom';
+
+import { useForm, FormProvider } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Form } from 'react-bootstrap';
+import TextField, { FieldGroup } from 'features/textField/TextField';
+import { DefaultLayout } from 'layout';
+
 // import localStorageUtil from 'utils/localStorageUtil';
 // import LocalStorageKeys from 'constants/localStorageKeys';
 import api from 'services/api';
+import routerPath from 'routes/router.path';
 
 import './postsPageCRUDStyle.scss';
 import { GetBackstagePostByIdClass } from './getBackstagePostByIdClass';
@@ -15,15 +24,38 @@ const _EHS = new ExceptionHandleService({
   _NOTICE: ''
 });
 
+const _defaultValues = {
+  title: '',
+  category: ''
+};
+// form 驗證
+const _schema = yup.object({
+  title: yup
+    .string()
+    .required('請輸入正確標題格式')
+    // .matches(/^[\u4E00-\u9FFFa-zA-Z]+$/, '請輸入正確標題格式')
+    .min(2, '請輸入大於2位中英字')
+    .max(50, '請輸入小於50位中英字')
+});
+
 /** 文章編輯頁  */
 const PostPageCRUD = () => {
   const mdStr = `# This is a H1  \n## This is a H2  \n###### This is a H6`;
 
   const location = useLocation();
+  const navigate = useNavigate();
   const [postTitle, setPostTitle] = useState(null);
   const [postCategory, setPostCategory] = useState(null);
   const [postCreateDate, setPostCreateDate] = useState(null);
   const [markdown, setMarkdown] = useState(mdStr);
+  const [formData, setFormData] = useState({});
+
+  const methods = useForm({
+    mode: 'onBlur',
+    defaultValues: _defaultValues,
+    resolver: yupResolver(_schema)
+  });
+  const { reset } = methods;
 
   const postId = useMemo(
     () => new URLSearchParams(location.search).get('id'),
@@ -53,8 +85,6 @@ const PostPageCRUD = () => {
   /** 初次載入 */
   const getInit = useCallback(async () => {
     try {
-      console.log('Page PostPage CRUD');
-
       const postInfo = await getPostById(postId);
       const postTitle = postInfo?.title;
       const postCategory = postInfo?.category;
@@ -64,31 +94,76 @@ const PostPageCRUD = () => {
       setPostCategory(postCategory);
       setPostCreateDate(postCreatedDate);
       setMarkdown(postContent);
+
+      const formateData = {
+        title: postTitle ?? '',
+        category: postCategory ?? ''
+      };
+
+      reset(formateData);
+      setFormData(formateData);
     } catch (error) {
       _EHS.errorReport(error, 'getInit', _EHS._LEVEL.ERROR);
     }
-  }, []);
+  }, [getPostById, postId, reset]);
 
   /** 初始化 */
   useEffect(() => {
     getInit();
   }, [getInit]);
 
+  /* 送出表單 */
+  const handleSubmit = async (data, e) => {
+    e.preventDefault();
+    const reqBody = {
+      title: data.title,
+      category: data.category,
+      content: markdown
+    };
+    console.log(reqBody);
+    // const res = await api.member.addMemberDetail(reqBody);
+    // if (res) {
+    //   await alert
+    //     .toast({ title: '新增會員成功' })
+    //     .then(() => navigate('/member/list'));
+    // }
+  };
+  const handleGoBack = () => {
+    navigate(
+      `/${routerPath.secretDoor}/${routerPath.secretDoor_Post}/${routerPath.secretDoor_Post_PostList}`
+    );
+  };
+
   return (
     <div id='post_page_crud' className='post_container'>
+      <FormProvider {...methods}>
+        {/* <DevTool control={methods.control} /> */}
+        <Form onSubmit={methods.handleSubmit(handleSubmit)}>
+          <DefaultLayout.Outlet onCancel={handleGoBack}>
+            <div className='form-grid form-grid-md-2'>
+              {/* 文章標題 */}
+              <FieldGroup title='文章標題' required htmlFor='title'>
+                <TextField name='title' maxLength='50' />
+              </FieldGroup>
+              {/* 文章分類 */}
+              <FieldGroup title='文章分類'>
+                <TextField name='category' disabled readOnly maxLength='50' />
+              </FieldGroup>
+            </div>
+            <div className='post_editor' data-color-mode='light'>
+              <MarkdownEditor
+                value={markdown}
+                height='60vh'
+                visible={true}
+                onChange={(value, viewUpdate) => {
+                  setMarkdown(value);
+                }}
+              />
+            </div>
+          </DefaultLayout.Outlet>
+        </Form>
+      </FormProvider>
       <dark-mode light='Light' dark='Dark'></dark-mode>
-      PostPage CRUD
-      <div className='post_editor' data-color-mode='light'>
-        <MarkdownEditor
-          value={markdown}
-          height='60vh'
-          width='100%'
-          onChange={(value, viewUpdate) => {
-            console.log('value:', value);
-            setMarkdown(value);
-          }}
-        />
-      </div>
     </div>
   );
 };
