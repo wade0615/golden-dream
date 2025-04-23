37ec52a3734dd908a550baf88ef52fec6b748d95
wade17 <wade_wu@17life.com>
2025-04-24 00:25:14 +0800

    feat: Google Analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import ReactGA from 'react-ga4';
 
 import router from './routes/router';
 // import PageRoute from 'pages/PagesRoute';
@@ -8,6 +10,15 @@ import Loading from 'components/loading/Loading';
 
 function App() {
   const isFetchLoading = useSelector((state) => state.loading);
+
+  useEffect(() => {
+    // 初始化 Google Analytics，將 'G-XXXXXXXXXX' 替換為你的測量 ID
+    ReactGA.initialize('G-6ZWSTSVNRP');
+
+    // 設定頁面檢視追蹤
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+  }, []);
+
   return (
     <>
       <Loading isLoading={isFetchLoading} />
