859ecf0441a98d0cbe9e586bd5c8e4f0852b43d7
wade17 <wade_wu@17life.com>
2025-04-24 13:48:31 +0800

    fix: Content-Security-Policy

--- server/src/Global/Middlewares/contentSecurityPolicy.middleware.ts
+++ server/src/Global/Middlewares/contentSecurityPolicy.middleware.ts
@@ -6,7 +6,7 @@ export class ContentSecurityPolicyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
     res.setHeader(
       'Content-Security-Policy',
-      "default-src 'self'; connect-src 'self' https://my-app-684523256629.asia-east1.run.app https://gorgeous-wade.com; script-src 'self'; font-src *; style-src * 'unsafe-inline'; img-src * data:;"
+      "default-src 'self'; connect-src 'self' https://my-app-684523256629.asia-east1.run.app https://gorgeous-wade.com; script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com; script-src-elem 'self' https://www.googletagmanager.com https://www.google-analytics.com; font-src *; style-src * 'unsafe-inline'; img-src * data:;"
     );
     console.log('ContentSecurityPolicyMiddleware executed');
     next();
