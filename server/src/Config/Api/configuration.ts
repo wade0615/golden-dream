3728763051688d378966f7421e4e19b3412eab91
Wade <wade@roddeiMac.local>
2025-06-13 10:04:23 +0800

    refactor: configuration.ts

--- server/src/Config/Api/configuration.ts
+++ server/src/Config/Api/configuration.ts
@@ -9,8 +9,5 @@ export default registerAs('api', () => ({
   jwtAccessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET,
   jwtAccessTokenExpirationTime: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
   jwtRefreshTokenSecret: process.env.JWT_REFRESH_TOKEN_SECRET,
-  jwtRefreshTokenExpirationTime: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
-  oAuthPublicKey: process.env.CRM_PUBLIC_KEY,
-  backstageChannelCertificate: process.env.BACKSTAGE_CHANNEL_CERTIFICATE,
-  oauthUrl: process.env.OAUTH_URL
+  jwtRefreshTokenExpirationTime: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME
 }));
