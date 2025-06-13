047e1ae422e48cbce0074dfdabe06c835828d314
Wade <wade@roddeiMac.local>
2025-06-13 14:02:49 +0800

    refactor: category module

--- server/src/Models/V1/Category/category.module.ts
+++ server/src/Models/V1/Category/category.module.ts
@@ -4,7 +4,6 @@ import { CategoryRepository } from './category.repository';
 import { CategoryService } from './category.service';
 
 import { MysqlModule } from 'src/Providers/Database/DatabaseMysql/mysql.module';
-// import { FirebaseModule } from 'src/Providers/Database/Firestore/firebase.module';
 
 @Module({
   imports: [MysqlModule],
